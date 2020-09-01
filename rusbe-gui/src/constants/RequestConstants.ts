fb2db653b4e1ce2448cab49bf83304bab567f7b2
Marconi Gomes <mgrf@cin.ufpe.br>
2020-08-31 21:49:57 -0300

    Atualizando URL de requisição nas constantes

--- rusbe-gui/src/constants/RequestConstants.ts
+++ rusbe-gui/src/constants/RequestConstants.ts
@@ -1,5 +1,5 @@
 export class RequestConstants {
-    REQUEST_URL: string = "http://localhost:3000"
+    REQUEST_URL: string = "http://localhost:3333"
     HEADER_CONTENT_JSON: string = "application/json";
 
     ROUTE_QUEUE: string = "/queue";
