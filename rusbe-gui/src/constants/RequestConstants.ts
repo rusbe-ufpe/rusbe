00b42bd9f2c6d1a01a3fe1a59c07f080371c0b61
Marconi Gomes <mgrf@cin.ufpe.br>
2020-08-31 22:00:16 -0300

    Adicionando headers comuns ao arquivo de constantes.

--- rusbe-gui/src/constants/RequestConstants.ts
+++ rusbe-gui/src/constants/RequestConstants.ts
@@ -1,6 +1,10 @@
+import {HttpHeaders} from "@angular/common/http";
+
 export class RequestConstants {
     REQUEST_URL: string = "http://localhost:3333"
+
     HEADER_CONTENT_JSON: string = "application/json";
+    DEFAULT_HEADERS: HttpHeaders = new HttpHeaders({"Content-type": this.HEADER_CONTENT_JSON});
 
     ROUTE_QUEUE: string = "/queue";
 }
