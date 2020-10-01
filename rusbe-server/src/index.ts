3232dc89a1da469b4ea1da38db06ff3c1c07cdad
Andre Luis <alpvj@cin.ufpe.br>
2020-10-01 16:43:08 -0300

    Adicionando os arquivos nas rotas

--- rusbe-server/src/index.ts
+++ rusbe-server/src/index.ts
@@ -1,22 +1,20 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import avaliacao from './routes/rating'
-import cardapio from './routes/menu'
-import fila from './routes/queue'
-import forum from './routes/forum'
-import grupos from './routes/groups'
-import sugestoes from './routes/suggestions'
+import menu from './services/menu.service'
+import queue from './services/queue.service'
+import threads from './services/threads.service'
+import groups from './services/groups.service'
+import suggestions from './services/suggestions.service'
 
 const app = express()
 app.use(express.json())
 app.use(bodyParser.json())
 
-app.use('/', avaliacao)
-app.use('/', cardapio)
-app.use('/', fila)
-app.use('/', forum)
-app.use('/', grupos)
-app.use('/', sugestoes)
+app.use('/', menu)
+app.use('/', queue)
+app.use('/', threads)
+app.use('/', groups)
+app.use('/', suggestions)
 
 app.listen(3333,()=>{
     console.log("Servidor ouvindo na porta 3333")
