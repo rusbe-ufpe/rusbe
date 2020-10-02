ae1f1804470d502247043b593b5853e0483119a3
Andre Luis <alpvj@cin.ufpe.br>
2020-10-01 23:11:03 -0300

    Pequeno ajuste no service de login da gui

--- rusbe-gui/src/service/Login.service.ts
+++ rusbe-gui/src/service/Login.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   constructor(public http: HttpClient) { }
+  private headers = new HttpHeaders({'Content-Type': 'application/json'});
   private pathUrl = 'http://localhost:3333/';
   login(credentials): Observable<any>{
-    return this.http.get(this.pathUrl + 'login'+'?id='+credentials.id+'&'+'password='+credentials.password, httpOptions);
+    return this.http.get(this.pathUrl + 'login'+'?id='+credentials.id+'&'+'password='+credentials.password, {headers: this.headers});
   }
 }
